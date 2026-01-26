49e2c05dd61007be4e7320ccabe751b8f1a8368b
tecnozoni <agustinminzoni@gmail.com>
2026-01-26 09:39:56 -0300

    volvio el codigo que tenia antes del astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import robotsTxt from 'astro-robots-txt'
-import sitemap from '@astrojs/sitemap'
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
 
 export default defineConfig({
-  site: 'https://agustinminzoni.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-    robotsTxt()
-  ]
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://agustinminzoni.com/'
 })
