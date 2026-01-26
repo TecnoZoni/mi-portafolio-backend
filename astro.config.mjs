c0f219374c39d3fc7935b101a759cf54898efe24
tecnozoni <agustinminzoni@gmail.com>
2026-01-25 21:21:23 -0300

    correccion para ver si el sitemap funciona

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://agustinminzoni.com',
   integrations: [
     tailwind(),
+    sitemap(),
     robotsTxt()
   ]
 })
